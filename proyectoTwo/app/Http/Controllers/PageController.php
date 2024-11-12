541cf6dc904cf5bd522b21d44d8d1cd8c67ade2b
Daniel Arias <danielsam707@gmail.com>
2024-11-12 15:49:05 -0500

    Se simplifico el archivo web.php y se agrego las funciones al controlador

--- proyectoTwo/app/Http/Controllers/PageController.php
+++ proyectoTwo/app/Http/Controllers/PageController.php
@@ -1,23 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers;// esta es la ruta de acceso a este controlador
+// la copiamos para poder trabar con ella en las rutas
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class PageController extends Controller 
 {
     public function home()
     {
-        
+        return view('home');
     }
 
     public function blog()
     {
-        
+        //consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+        return view('blog', ['posts' => $posts]); //aqui solo retorna el texto
     }
 
     public function post($slug)
     {
-        
+        // consulta a base de datos
+        $post = $slug;
+        return view('post', ['post' => $post]);
     }
 }
--- proyectoTwo/routes/web.php
+++ proyectoTwo/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request; // Es para manejar solicitudes o peticiones de usuario
+use App\Http\Controllers\PageController;
 
 /**
  * Route::get    |Consultar
@@ -10,34 +11,23 @@ use Illuminate\Http\Request; // Es para manejar solicitudes o peticiones de usua
  * Route::put    |Actualizar
  */
 
-/** 
-Route::get('/', function () { // Lo que dice que "/" es que si vamos a la raiz obtendremos una vista llamada welcome
-    return view('welcome');
+
+/**
+Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('blog', [PageController::class, 'blog'])->name('blog');
+
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+
+ */
+
+
+
+//En este punto formo un grupo de clase y llamo la clase una sola vez y en las rutas invocos
+// los metodos de cada vista
+Route::controller(PageController::class)->group(function()
+{
+    Route::get('/',           'home')->name('home');
+    Route::get('blog',        'blog')->name('blog');
+    Route::get('blog/{slug}', 'post')->name('post');
 });
-
-
-Route::get('/', function () {
-    return 'Ruta Home'; //aqui no utilizamos la funcion view solo un return entre comillas simples y eso es lo que mostrara la vista
-});
-*/
-
-Route::get('/', function () {
-    return view('home'); //aqui ya se maneja retorno con una funcion para vistas
-})->name('home');
-
-Route::get('blog', function () {
-    //consulta a base de datos
-    $posts = [
-        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
-        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
-    ];
-    return view('blog', ['posts' => $posts]); //aqui solo retorna el texto
-})->name('blog');
-
-/**Estas dos antreires on las rutas mas basicas, aceptan un nombre y derectamente una funcion anonima */
-
-Route::get('blog/{slug}', function ($slug) {
-    // consulta a base de datos
-    $post = $slug;
-    return view('post', ['post' => $post]);
-})->name('post');
