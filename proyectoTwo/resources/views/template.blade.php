ee2f0f57547ebf3ca115a606259de7d5aa15f277
Daniel Arias <danielsam707@gmail.com>
2024-11-13 16:11:21 -0500

    clase 13: se crea el inicio de sesion y se instalan npm, breeze y se ejecutan los comandos composer require laravel/breeze --dev, npm install, npm run dev

--- proyectoTwo/resources/views/template.blade.php
+++ proyectoTwo/resources/views/template.blade.php
@@ -9,10 +9,17 @@
     <p>
         <a href="{{ route('home')}}">Home</a>
         <a href="{{ route('blog')}}">Blog</a>
+
+        @auth
+        <a href="{{ route('dashboard') }}">Dashboard</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
+        @endauth
     </p>
 
     <hr>
 
     @yield('content')
+
 </body>
 </html>
--- proyectoTwo/routes/web.php
+++ proyectoTwo/routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request; // Es para manejar solicitudes o peticiones de usuario
 use App\Http\Controllers\PageController;
-
-/**
- * Route::get    |Consultar
- * Route::post   |Guardar
- * Route::delete |Eliminar
- * Route::put    |Actualizar
- */
-
-
-/**
-Route::get('/', [PageController::class, 'home'])->name('home');
-
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
-
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
-
- */
 
 
 
@@ -31,3 +15,21 @@ Route::controller(PageController::class)->group(function()
     Route::get('blog',        'blog')->name('blog');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+
+
+
