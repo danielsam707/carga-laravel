d943829472f93a5fd6a18ff2e0e3207988192c67
Daniel Arias <danielsam707@gmail.com>
2024-11-14 17:02:11 -0500

    Clase 16: Listado de publicaciones | se elabora la vista del post y se le pasan todos los registros de pots por medio del controlador en la vista.

--- proyectoTwo/app/Http/Controllers/PostController.php
+++ proyectoTwo/app/Http/Controllers/PostController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
