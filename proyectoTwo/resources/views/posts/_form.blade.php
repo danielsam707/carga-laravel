51aff0a49e637feb5e7b080ec03cccb8e7530377
Daniel Arias <danielsam707@gmail.com>
2024-11-15 10:44:32 -0500

    Clase 19: controles de un formulario | Se modifica el metodo create() de PostController, se crea el formulario con estilos

--- proyectoTwo/resources/views/posts/_form.blade.php
+++ proyectoTwo/resources/views/posts/_form.blade.php
@@ -1,1 +1,13 @@
-Formulario
+@csrf 
+
+<label class="uppercase text-gray-700 text-xs">Titulo</label>
+<input type="text" name="title" class="text-black rounded border-200 w-full mb-4" value="{{ $post->title }}" >
+
+<label class="uppercase text-gray-700 text-xs">Contenido</label>
+<textarea name="body"rows="5" class="text-black rounded border-200 w-full mb-4" >{{ $post->body }}</textarea>
+
+<div class="flex justify-between item-center">
+  <a href="{{ route('posts.index')}}" class="text-indigo-600">Volver</a>
+
+  <input type="submit" class="bg-red-800 text-white rounded px-4 py-2">
+</div>
