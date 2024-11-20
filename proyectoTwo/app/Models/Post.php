8f4d3fce79a1f78e537144103f73c4d984c6c747
Daniel Arias <danielsam707@gmail.com>
2024-11-20 14:25:25 -0500

    Clase 23: Registros duplicados | Se modifican los metodod strore() y update() de PostController, en la parte donde crean y editan los post despues de las validaciones - Se modifica el modelo agreganto la propiedad getRouteKeyName() para que se busque en la base de datos por el slug y no por el id - Se modifico la vista _form para agregar el imput slug con su lable y mensajes de error.

--- proyectoTwo/app/Models/Post.php
+++ proyectoTwo/app/Models/Post.php
@@ -14,6 +14,11 @@ class Post extends Model
         'body',
     ];
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
--- proyectoTwo/resources/views/posts/_form.blade.php
+++ proyectoTwo/resources/views/posts/_form.blade.php
@@ -4,6 +4,10 @@
 <span class="text-xs text-red-600">@error('title') {{$message}} @enderror</span>
 <input type="text" name="title" class="text-black rounded border-200 w-full mb-4" value="{{ old('title', $post->title) }}" >
 
+<label class="uppercase text-gray-700 text-xs">Slug</label>
+<span class="text-xs text-red-600">@error('slug') {{$message}} @enderror</span>
+<input type="text" name="slug" class="text-black rounded border-200 w-full mb-4" value="{{ old('slug', $post->slug) }}" >
+
 <label class="uppercase text-gray-700 text-xs">Contenido</label>
 <span class="text-xs text-red-600">@error('body') {{$message}} @enderror</span>
 <textarea name="body"rows="5" class="text-black rounded border-200 w-full mb-4" >{{ old('body',$post->body) }}</textarea>
